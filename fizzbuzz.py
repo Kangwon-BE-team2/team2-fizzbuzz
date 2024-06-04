dc45f539b6b92e46fa2ba5a101f0efaa83226ada
UiChan Lee <162671129+ana-raka@users.noreply.github.com>
2024-06-04 20:12:56 +0900

    Update: fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,13 +1,12 @@
 def fizzbuzz(n):
-  for i in range(1, 101):
-    if i % 3 == 0:
+    if n % 3 == 0:
       print('Fizz')
-    elif(i % 5 == 0) :
+    elif(n % 5 == 0) :
       print("Buzz") 
-     elif i % 3 == 0 and i % 5 ==0:   
+     elif n % 3 == 0 and n % 5 ==0:   
       print('fizzbuzz')
     else:
-      print(i)
+      print(n)
 def main():
   for i in range(1,101):
     print(fizzbuzz(i))
